946b2754ceaaf6cee46f9ebf0767764f5bf1a758
Princelo <lamkimcheung@gmail.com>
2015-07-27 14:25:19 +0800

    edit menu

--- admin/index.php
+++ admin/index.php
@@ -132,7 +132,6 @@ elseif ($_REQUEST['act'] == 'menu')
         }
 
     }
-
     $smarty->assign('menus',     $menus);
     $smarty->assign('no_help',   $_LANG['no_help']);
     $smarty->assign('help_lang', $_CFG['lang']);
